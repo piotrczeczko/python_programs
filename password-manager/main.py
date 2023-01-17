7019d8c32a237d7ba6757717900a5e6b7c8cc253
Piotr Czeczko <pczeczko@rocketlawyer.co.uk>
2023-01-17 11:41:50 +0000

    added search and exceptions

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,8 +1,10 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 
+FILE_NAME = "data.json"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -31,21 +33,52 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    s_website = entry_website.get()
-    s_username = entry_username.get()
-    s_password = entry_password.get()
+    website = entry_website.get()
+    username = entry_username.get()
+    password = entry_password.get()
+    new_data = {
+        website: {
+            "email": username,
+            "password": password,
+        }
+    }
 
-    if len(s_website) == 0 or len(s_username) == 0 or len(s_password) == 0:
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="None of the fields should be empty!")
     else:
-        is_ok = messagebox.askokcancel(title=s_website, message=f"These are the details entered: \nEmail: {s_username} "
-                               f"\n Password: {s_password} \n Is it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{s_website} | {s_username} | {s_password}\n")
-            # clear fields:
+        try:
+            with open(FILE_NAME, "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open(FILE_NAME, "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open(FILE_NAME, "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             entry_website.delete(0, END)
             entry_password.delete(0, END)
+
+
+def find_password():
+    website = entry_website.get()
+    if len(website) == 0:
+        messagebox.showinfo(title="Oops", message="Website shouldn't be empty!")
+    else:
+        try:
+            with open(FILE_NAME, "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data File Found")
+        else:
+            if website in data:
+                messagebox.showinfo(title=website, message=f"Email: {data[website]['email']}\nPassword: {data[website]['password']}")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for website: {website}")
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -61,28 +94,31 @@ window.config(padx=50, pady=50, bg=WHITE)
 canvas = Canvas(width=200, height=200, highlightthickness=0, bg=WHITE)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0,column=1)
+canvas.grid(row=0, column=1)
+###
 label_website = Label(text="Website:", bg=WHITE, fg=FONT_COLOR, font=(FONT_NAME, FONT_SIZE))
 label_website.grid(row=1, column=0, sticky=NE)
-label_username = Label(text="Email/Username:", bg=WHITE, fg=FONT_COLOR, font=(FONT_NAME, FONT_SIZE))
-label_username.grid(row=2, column=0, sticky=NE)
-label_passwrd = Label(text="Password:", bg=WHITE, fg=FONT_COLOR, font=(FONT_NAME, FONT_SIZE))
-label_passwrd.grid(row=3, column=0, sticky=E)
-
-entry_website = Entry(width=43)
+entry_website = Entry(width=24)
 entry_website.insert(END, string="")
-entry_website.grid(row=1, column=1, columnspan=2)
+entry_website.grid(row=1, column=1)
+button_search = Button(text="Search", width=16, command=find_password, highlightthickness=0)
+button_search.grid(row=1, column=2, sticky=NW)
+###
+label_username = Label(text="Email/Username:",bg=WHITE, fg=FONT_COLOR, font=(FONT_NAME, FONT_SIZE))
+label_username.grid(row=2, column=0, sticky=NE)
 entry_username = Entry(width=43)
 entry_username.insert(0, string="piotr.czeczko@gmail.com")
 entry_username.grid(row=2, column=1, columnspan=2)
+###
+label_passwrd = Label(text="Password:", bg=WHITE, fg=FONT_COLOR, font=(FONT_NAME, FONT_SIZE))
+label_passwrd.grid(row=3, column=0, sticky=E)
 entry_password = Entry(width=24)
 entry_password.insert(END, string="")
 entry_password.grid(row=3, column=1, sticky=E)
-button_generate_password = Button(text="Generate Password", command=generate_password, highlightthickness=0)
+button_generate_password = Button(text="Generate Password", width=16, command=generate_password, highlightthickness=0)
 button_generate_password.grid(row=3, column=2, sticky=NW)
+###
 button_add = Button(text="Add", width=41, command=save, highlightthickness=0)
 button_add.grid(row=4, column=1, columnspan=2)
-
-
 
 window.mainloop()
